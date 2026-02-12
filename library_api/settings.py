8bf8273c0bceb57b5698d4ae72ed28cb60e04c44
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2026-02-12 15:22:14 +0600

    feat: Configure JWT authentication and Djoser settings

--- library_api/settings.py
+++ library_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,8 +147,6 @@ REST_FRAMEWORK = {
 }
 
 # JWT Settings
-from datetime import timedelta
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
