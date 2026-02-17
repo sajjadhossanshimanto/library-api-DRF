8acabfd3fff7fade138e8dcdc06b1c58c76618df
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2026-02-17 23:20:05 +0600

    vercel config

--- library_api/settings.py
+++ library_api/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&qooqxm1z3+i2osdxg2f%8bctk2+r4@++vkkz9zn59q%hppzt!'
+SECRET_KEY = config('django_secrect')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'library_api.wsgi.application'
+WSGI_APPLICATION = 'library_api.wsgi.app'
 
 
 # Database
