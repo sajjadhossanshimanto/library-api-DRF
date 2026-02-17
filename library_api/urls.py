744d1e4ba4643f70798f04d7160f1099a6094536
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2026-02-18 02:21:47 +0600

    Disable authentication for schema view

--- library_api/urls.py
+++ library_api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.views.decorators.csrf import csrf_exempt
 
 # Swagger/OpenAPI Schema
 schema_view = get_schema_view(
@@ -16,6 +17,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    authentication_classes=[],
 )
 
 urlpatterns = [
